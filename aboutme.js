6a9331910eef987981ff3474bf86446bd26cfa1d
corgidotcom <me@corgidot.com>
2022-10-23 11:15:22 -0700

    space button to control music + changelog

--- aboutme.js
+++ aboutme.js
@@ -13,8 +13,35 @@ window.onload = () => {
     } else {
         musicbutton.setAttribute('src', "media/pause.png")
     }
+    document.addEventListener('keyup', event => {
+        if (event.code === 'Space') {
+            console.log('Space pressed');
+            if(audio.paused == false)
+            {
+                audio.pause();
+                musicbutton.setAttribute('src', "media/play.png")
+            }
+            else
+            {
+                audio.play();
+                musicbutton.setAttribute('src', "media/pause.png")
+            }
+        }
+    })
 
     musicbutton.onclick = function(){
+        if(audio.paused == false)
+        {
+            audio.pause();
+            musicbutton.setAttribute('src', "media/play.png")
+        }
+        else
+        {
+            audio.play();
+            musicbutton.setAttribute('src', "media/pause.png")
+        }
+    }
+    Onbutton = function(){
         if(audio.paused == false)
         {
             audio.pause();
