0a8be7e8ffe7a935ca09cd11945885d2a5518ae2
corgidotcom <me@corgidot.com>
2022-10-22 20:34:45 -0700

    JAVASCRIPT YAY now you can pause the music

---
+++ aboutme.js
@@ -0,0 +1,24 @@
+let musicbutton = document.querySelector('.musicbutton')
+var audio = document.getElementById("legendarymember");
+audio.volume = 0.2
+audio.paused = false
+
+musicbutton.onclick = function(){
+    let mySrc = musicbutton.getAttribute("src");
+    if(mySrc == "media/pause.png")
+    {
+        musicbutton.setAttribute('src', "media/play.png")
+    }
+    else
+    {
+        musicbutton.setAttribute('src', "media/pause.png")
+    }
+    if(audio.paused == false)
+    {
+        audio.pause();
+    }
+    else
+    {
+        audio.play();
+    }
+}
